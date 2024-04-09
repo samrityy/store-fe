c012d3e794082e621cef6da655b7adfdc3896893
Samrityy <smritymulmi23@gmail.com>
2024-04-09 16:17:39 +0545

    feat: add composable in register page

--- src/composables/useRegisterForm.js
+++ src/composables/useRegisterForm.js
@@ -1,14 +1,34 @@
-import { ref } from 'vue';
-export function useRegisterForm() {
 
-  const name= ref('');
+import { ref } from 'vue';
+import { useFetch } from './fetch.js';
+
+export function useRegisterForm() {
+  const name = ref('');
   const email = ref('');
   const password = ref('');
+  const data = ref(null);
+  const error = ref(null);
+
   const submitForm = () => {
-    console.log('Form submitted:', { name:name.value ,email: email.value, password: password.value });
-    name.value = '';
-    email.value = '';
-    password.value = '';
+    console.log('submitForm');
+    try {
+      const formData = {
+        name: name.value,
+        email: email.value,
+        password: password.value
+      };
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: formData
+      };
+      const { data, error } = useFetch('http://127.0.0.1:8000/user/register/', options)
+      console.log('Registration successful!');
+    } catch (error) {
+      console.error('Error during registration:', error);
+    }
   };
-  return { name,email, password, submitForm };
+  return { name, email, password, "submitForm" : submitForm, data, error};
 }
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -8,16 +8,16 @@
                     <h3 class="text-lg">
                         Create an account
                     </h3>
-                    <form action="">
+                    <form @submit.prevent="submitForm" method="post"> 
                         <label for="name" class="block mb-2 text-left text-gray-700 font-bold">Name:</label>
-                        <input type="text" id="name" name="password" placeholder="Enter your full name" class="block w-full mb-6 px-4 py-2 border border-gray-300 rounded-md focus:outline-none
+                        <input v-model="name" type="text" id="name" name="password" placeholder="Enter your full name" class="block w-full mb-6 px-4 py-2 border border-gray-300 rounded-md focus:outline-none
                             focus:border-green-400" required>
                         <label for="email" class="block mb-2 text-left text-gray-700 font-bold">Email:</label>
-                        <input type="email" id="password" name="password" placeholder="Enter your email" class="block w-full mb-6 px-4 py-2 border border-gray-300 rounded-md focus:outline-none
+                        <input v-model="email" type="email" id="password" name="password" placeholder="Enter your email" class="block w-full mb-6 px-4 py-2 border border-gray-300 rounded-md focus:outline-none
                             focus:border-green-400" required>
             
                         <label for="password" class="block mb-2 text-left text-gray-700 font-bold">Password:</label>
-                        <input type="password" id="password" name="password" placeholder="Enter your Password" class="block w-full mb-6 px-4 py-2 border border-gray-300 rounded-md focus:outline-none
+                        <input v-model="password" type="password" id="password" name="password" placeholder="Enter your Password" class="block w-full mb-6 px-4 py-2 border border-gray-300 rounded-md focus:outline-none
                             focus:border-green-400" required>
             
                         <div class="flex justify-center items-center">
@@ -34,8 +34,7 @@
     </layout-div>
  </template>
 <script setup>
-import { useRegisterForm } from '/home/smrity/store-fe/src/composables/useRegisterForm.js';
-   
-const { email, password, submitForm } = useRegisterForm();
+import { useRegisterForm } from '@/composables/useRegisterForm.js';
+const { name, email, password, submitForm, data, error } = useRegisterForm();
 </script>
  
