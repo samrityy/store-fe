bb8f7b069c38a6916cad4c4aa38a541d87979ec8
Samrityy <smritymulmi23@gmail.com>
2024-04-11 15:27:29 +0545

    fix: retur promise

--- src/composables/fetch.js
+++ src/composables/fetch.js
@@ -1,15 +1,19 @@
-import { ref } from 'vue';
+
 
 export function useFetch(url, options) {
-  const data = ref(null);
-  const error = ref(null);
-  // return new Promise((resolve, reject) => {
-    try {
-      fetch(url, options)
-    }
-    catch(err){
-      console.log('error')
-    }
+  return new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+}
+
       // .then((res) => res.json())
       // .then((json) => {
       //   data.value = json;
@@ -30,8 +34,6 @@ export function useFetch(url, options) {
   // }catch(err){
   //   error.value = err;
   // }
-  return { data, error };
-}
   // fetch(url, options)
 //     .then((res) => res.json())
 //     .then((json) => (data.value = json))
