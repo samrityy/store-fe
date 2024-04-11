237d4b80da487dc3b7af0783dd1db0c80c345778
Samrityy <smritymulmi23@gmail.com>
2024-04-11 15:27:51 +0545

    fix: minor chnages

--- src/composables/useLoginForm.js
+++ src/composables/useLoginForm.js
@@ -1,35 +1,39 @@
-
 import { ref } from 'vue';
 import { useFetch } from './fetch.js';
 
-export function useLoginForm(){
-  const email=ref('');
-  const password=ref('');
-  const data=ref(null);
-  const error=ref(null);
+export function useLoginForm() {
+  const email = ref('');
+  const password = ref('');
+  const data = ref(null);
+  const error = ref(null);
 
-  const submitForm=async()=>{
+  const submitForm = async () => {
     console.log('submitForm');
-    try{
-      const formData={
-        email:email.value,
-        password:password.value
-      };
-      const options={
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json'
-        },
-        body:JSON.stringify(formData)
-      };
-      const {data:responseData,error:responseError}=await useFetch('http://127.0.0.1:8000/user/register/', options);
-      return {responseData, responseError};
-    }
-    catch(err){
-      error.value=err;
+
+    try {
+          const formData = {
+          email: email.value,
+          password: password.value
+          };
+
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(formData)
+           };
+        const { data:responseData} = await useFetch('http://127.0.0.1:8000/user/login/', options);
+        data.value = responseData;
+
+      console.log('Login  successful!', responseData);
+      return { responseData};
+    
+    } catch (err) {
+      error.value = err;
       console.error('Error during login:', err);
-      throw(err);
+      throw(err)
     }
   };
-  return { email, password, submitForm};
+  return {email, password, submitForm};
 }
