7f8bf8d5397af4aff6420b6592bfe16cf2d61108
Samrityy <smritymulmi23@gmail.com>
2024-04-11 15:28:10 +0545

    feat: add header

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,9 +1,16 @@
 <template>
-    <div class="bg-custom-brown min-h-screen">
-    <router-view/>
+    <div class="bg-custom-brown h-screen w-screen">
+        <div class="p-4">
+            <a href="#" class="text-white text-l  p-4">Home</a>
+            <a href="#" class="text-white text-l  p-4">Contact</a>
+            <a href="#" class="text-white text-l  p-4">About</a>
+            <a href="/login" class="text-white text-l  p-4">Login</a>
+            <a href="#" class="text-white text-2l  p-4">Logout</a>
+        </div>
+        
+        <router-view/>
     </div>
 </template>
-
 <script>
     export default{
         name: 'MainLayout'
