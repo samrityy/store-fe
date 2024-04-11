98d1da6f451cc783cc2e76dbf4d27913153b1a50
Samrityy <smritymulmi23@gmail.com>
2024-04-11 17:38:26 +0545

    fix: changes on error handling in useFetch

--- src/composables/fetch.js
+++ src/composables/fetch.js
@@ -3,14 +3,13 @@
 export function useFetch(url, options) {
   return new Promise((resolve, reject) => {
     fetch(url, options)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data)
       })
-      .then((data) => resolve(data))
-      .catch((error) => reject(error));
+      .catch((error) => {
+        reject(error)
+      });
   });
 }
 
