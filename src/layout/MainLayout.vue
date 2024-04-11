f5c02fd96f9ba9e5c8e6e3356ef4796d9553e43b
Samrityy <smritymulmi23@gmail.com>
2024-04-11 17:39:31 +0545

    fix: added more components in MainLayout

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="bg-custom-brown h-screen w-screen">
-        <div class="p-4">
-            <a href="#" class="text-white text-l  p-4">Home</a>
-            <a href="#" class="text-white text-l  p-4">Contact</a>
-            <a href="#" class="text-white text-l  p-4">About</a>
-            <a href="/login" class="text-white text-l  p-4">Login</a>
-            <a href="#" class="text-white text-2l  p-4">Logout</a>
+        <div class="p-3">
+            <a href="/" class="text-white text-l  p-3">Home</a>
+            <a href="/dashboard" class="text-white text-l  p-3">Products</a>
+            <a href="#" class="text-white text-l  p-3">About</a>
+            <a href="/user/login/" class="text-white text-l  p-3">Login</a>
+            <a href="/user/register" class="text-white text-2l  p-4">Register</a>
+            <div class="bg-custom-browm text-white p-4">
+        <h1 class="text-center text-2xl font-extrabold">MIMI STORE</h1>
+    </div>
         </div>
         
         <router-view/>
