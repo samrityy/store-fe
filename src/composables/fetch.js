63b518caec0c9ea48a9d20627da1b8d9009db472
Samrityy <smritymulmi23@gmail.com>
2024-04-14 12:58:37 +0545

    fix: removed commented code

--- src/composables/fetch.js
+++ src/composables/fetch.js
@@ -1,5 +1,3 @@
-
-
 export function useFetch(url, options) {
   return new Promise((resolve, reject) => {
     fetch(url, options)
@@ -12,30 +10,3 @@ export function useFetch(url, options) {
       });
   });
 }
-
-      // .then((res) => res.json())
-      // .then((json) => {
-      //   data.value = json;
-      //   resolve(data, error);
-      // })
-  // });
-  // const data = ref(null);
-  // const error = ref(null);
-  
-  // try{
-  // // if (options && options.body) options.body = JSON.stringify(options.body);
-  // const res = await fetch(url, options);
-  // if (res.status > 399) {
-  //   error.value = res;
-  // }
-  // const json = await res.json();
-  // data.value = json;
-  // }catch(err){
-  //   error.value = err;
-  // }
-  // fetch(url, options)
-//     .then((res) => res.json())
-//     .then((json) => (data.value = json))
-//     .catch((err) => (error.value = err));
-//   return { data, error };
-// }
