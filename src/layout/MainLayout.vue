63e2c01d297f29ebcb1f0c176da4788dd50f6b75
Samrityy <smritymulmi23@gmail.com>
2024-04-15 16:35:43 +0545

    feat: remove localstorage token when logout

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,21 +1,44 @@
 <template>
-    <div class="bg-custom-brown h-screen w-screen">
+    <div class="bg-custom-brown h-screen w-screen overflow-auto">
         <div class="p-3">
             <a href="/" class="text-white text-l  p-3">Home</a>
-            <a href="/products" class="text-white text-l  p-3">Products</a>
+            <!-- <a href="/products" class="text-white text-l  p-3">Products</a> -->
             <a href="#" class="text-white text-l  p-3">About</a>
             <a href="/user/login/" class="text-white text-l  p-3">Login</a>
             <a href="/user/register" class="text-white text-2l  p-4">Register</a>
+            <button @click="handleLogout" class="text-white">Logout</button>
             <div class="bg-custom-browm text-white p-4">
-        <h1 class="text-center text-2xl font-extrabold">MIMI STORE</h1>
+        <h1 class="text-center text-4xl font-extrabold">MIMI STORE</h1>
     </div>
         </div>
         
         <router-view/>
     </div>
 </template>
-<script>
-    export default{
-        name: 'MainLayout'
+
+<script setup>
+import { useFetch } from '@/composables/fetch.js';
+import router from '@/router/router.js';
+
+const handleLogout = async () => {
+  try {
+    const token = localStorage.getItem('token');
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token ${token}',
+      },
     };
+
+     await useFetch('http://127.0.0.1:8000/user/logout/', options).then((response) => {
+      console.log('-------------------log-------------')
+      localStorage.removeItem('token');
+      localStorage.removeItem('email');
+      router.push({ name: 'Login' });
+    });
+    }catch (error) {
+    console.error('Logout failed:', error);
+  }
+};
 </script>
