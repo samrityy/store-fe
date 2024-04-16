20d5c93a26b7941e24bdd561a9d9bbe73aaf9284
Samrityy <smritymulmi23@gmail.com>
2024-04-16 17:31:51 +0545

    feat:add toast

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue';
 import './assets/styles.css';
 import App from './App.vue';
 import router from './router/router.js'; 
-
-createApp(App).use(router).mount('#app');
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+const app = createApp(App);
+app.use(router);
+app.use(Toast, {
+  position: 'center',
+  icon: true,
+  transition: "Vue-Toastification__bounce",
+});
+app.mount('#app');
