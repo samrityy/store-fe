7266eb43db18c98fcd591937298296132b778b84
Samrityy <smritymulmi23@gmail.com>
2024-04-19 17:44:37 +0545

    feat: creat a component for input field

---
+++ src/components/TextInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <label class="block mb-2 text-left text-gray-700 font-bold">{{ label }}</label>
+        <input
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            v-bind="$attrs"
+            class="block w-full mb-6 px-4 py-2 border border-gray-300 rounded-md focus:outline-none
+                            focus:border-custom-brown"
+
+            />
+    </div>
+</template>
+
+<script setup>
+    const props=defineProps({
+        label:{
+            type: String,
+            default:''
+        },
+        modelValue:{
+            type: [String,Number],
+            default:''
+        },
+        
+    })
+</script>
