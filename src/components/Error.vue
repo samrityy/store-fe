4a9868435a93968ce932ae857e15db3617f984f2
Samrityy <smritymulmi23@gmail.com>
2024-04-19 17:45:58 +0545

    fix: create a component for viewing error

---
+++ src/components/Error.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex">
+    <div class="mr-auto text-red-500">{{ errorMessage}}</div>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+ const props = defineProps({
+     errorMessage: {
+         type: String,
+         default: ''
+     }
+ })
+</script>
