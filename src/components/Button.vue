ebc823f3a77dd4ff8cbf06a18d478f419ffe2ac2
Samrityy <smritymulmi23@gmail.com>
2024-04-19 17:45:00 +0545

    feat: create a component for button field

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button
+        class="bg-custom-brown text-white py-3 px-6 rounded-md cursor-pointer transition-colors 
+                                duration-300 hover:custom-brown"
+            @click="submitForm">{{ buttonText }}</button>
+    </div>
+</template>
+<script setup>
+const props=defineProps({
+        buttonText:{
+            type:String,
+        }  
+    })
+</script>
