db37ae01f808bbe6b3ec388e162d249fd1036c52
Samrityy <smritymulmi23@gmail.com>
2024-04-25 17:08:33 +0545

    feat: cart into the header

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -6,7 +6,9 @@
             <a href="#" class="text-white text-l  p-3">About</a>
             <a href="/user/login/" class="text-white text-l  p-3">Login</a>
             <a href="/user/register" class="text-white text-2l  p-4">Register</a>
+            <a href="/cart" class="text-white text-l  p-3">Cart</a>
             <button @click="handleLogout" class="text-white">Logout</button>
+        
             <div class="bg-custom-browm text-white p-4">
         <h1 class="text-center text-4xl font-extrabold">MIMI STORE</h1>
     </div>
