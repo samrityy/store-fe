9ae97175cb1d03d782c12d7b7635555544fad241
Samrityy <smritymulmi23@gmail.com>
2024-04-25 17:09:16 +0545

    feat: add delete in Cart page

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -7,14 +7,15 @@
         <li v-for ="(prod,index) in get_data" :key="index">
         <div  class="bg-custom-light rounded-lg p-4 shadow-md"> 
            <img :src="prod.product.image" :alt="prod.product.title" class="w-16 h-16 object-cover mb-4">
-          <h3 class="text-mg font-semibold mb-2 h-18">{{ prod.product.title }}</h3>
+          <h3 class="text-mg font-semibold mb-2 h-18">{{prod.product.id }}{{ prod.product.title }}</h3>
          <p class="text-gray-800 font-semibold mb-2">${{ prod.product.price }}</p>
           <button class="bg-custom-brown text-white py-2 px-4 rounded-md cursor-pointer transition-colors duration-300 hover:custom-brown" @click="decrement()">-</button>
           <span class="p-3"> {{ prod.quantity}}</span>
           <button class="bg-custom-brown text-white py-2 px-4 rounded-md cursor-pointer transition-colors 
-                                 duration-300 hover:custom-brown" @click="increment(prod.product,index)"> + </button>  
+            duration-300 hover:custom-brown" @click="increment(product,index)"> + </button>  
              
-          
+          <button class="bg-custom-brown text-white py-2 px-4 m-8 rounded-md cursor-pointer transition-colors 
+            duration-300 hover:custom-brown" @click="delete_from_cart(product,index)"> X </button> 
           </div>
         </li>
           </ul>
@@ -27,27 +28,32 @@
 import {computed} from 'vue';
 import { useCart } from "@/composables/useCart"
 import {get_all_cart} from "@/composables/useCart"
+import { data } from 'autoprefixer';
 const cart = useCart()
 const items = cart.items
-const get_items=cart.get_items
 const quantity = cart.quantity
 const removeFromCart = cart.removeFromCart
-const total = computed(() => {  
-  let totalPrice = 0; 
-  for (let i = 0; i < items.length; i++) {
-    totalPrice += Number(items[i].price)*quantity[items[i].id];
-  }
-  return totalPrice;
-});
+
 console.log("items",items)
-// const increment = (product,index) => {
-//    cart.increment(items[index],index);
-//  }
+
   const decrement = () => {
     cart.decrement();
   }
 
-console.log('Total',total)
 const{get_data,error}=get_all_cart()  
-console.log('Get Cart',get_data)
+
+const delete_from_cart = (product, index) => {
+   cart.delete_from_cart(get_data.value[index], index);  
+} 
+const increment = (product,index) => {
+    cart.increment(get_data.value[index],index);
+  }
+
+  const total = computed(() => {  
+  let totalPrice = 0; 
+  for (let i = 0; i < items.length; i++) {
+    totalPrice += Number(get_data.value[i].price)*quantity[get_data.value[i].id];
+  }
+  return totalPrice;
+});
 </script>
